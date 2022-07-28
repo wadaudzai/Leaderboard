d42a05e543ea775629094fe444c51289fa914a8c
wadaudzai <wadaudzai@gmail.com>
2022-07-28 21:11:44 +0430

    Hit the API

--- src/modules/show.js
+++
@@ -1,8 +0,0 @@
-import createDynamic from './scores.js';
-
-const showScores = () => {
-  const ul = document.querySelector('.scores');
-  ul.innerHTML = createDynamic();
-};
-
-export default showScores;
